5362ab73bee851dc8ed42f112d11911ff6f1c6ac
SHREE0778 <shreeramllts44@gmail.com>
2025-12-17 09:20:22 +0530

    commited to second branch

--- config/settings.py
+++ config/settings.py
@@ -1,14 +1,21 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-
 load_dotenv()
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-change-this-in-production')
 
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
+
+from sentence_transformers import SentenceTransformer
+
+def load_embedder():
+    return SentenceTransformer(
+        "all-MiniLM-L6-v2",
+    )
 
 ALLOWED_HOSTS = ['*']
 
@@ -88,12 +95,24 @@ LOGOUT_REDIRECT_URL = 'chatbot:login'
 
 # RAG Settings
 CHROMA_PERSIST_DIRECTORY = BASE_DIR / 'docs' / 'chroma'
+from pathlib import Path
+
+CHROMA_PERSIST_DIRECTORY = BASE_DIR / 'docs' / 'chroma'
+CHROMA_PERSIST_DIRECTORY.mkdir(parents=True, exist_ok=True)
+
 GROQ_API_KEY = os.getenv('GROQ_API_KEY')
 LLM_MODEL = os.getenv('LLM_MODEL', 'llama-3.1-8b-instant')
 EMBEDDING_MODEL = 'all-MiniLM-L6-v2'
 CHUNK_SIZE = 500
 CHUNK_OVERLAP = 50
 
+import os
+
+# Force CPU-only for sentence-transformers (important for Railway)
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+os.environ['OMP_NUM_THREADS'] = '2'
+
+# PRODUCTION SETTINGS
 # ============================================
 # PRODUCTION SETTINGS
 # ============================================
@@ -101,7 +120,7 @@ CHUNK_OVERLAP = 50
 import dj_database_url
 
 # Allow Railway domain
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','.railway.app']
 
 # Database - Use PostgreSQL in production
 if not DEBUG:
@@ -112,7 +131,8 @@ if not DEBUG:
     )
 
 # Static files
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # WhiteNoise - Serve static files
