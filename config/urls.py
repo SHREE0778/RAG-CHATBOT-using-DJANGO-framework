c96202e7ca54c3f2ee2eec2e2a4056b3f9a69c05
SHREE0778 <shreeramllts44@gmail.com>
2025-12-18 14:44:56 +0530

    changes made

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', RedirectView.as_view(url='accounts/login/', permanent=False)),
-    path('chatbot/', include('chatbot.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    # Use chatbot app URLs (includes login, logout, register)
+    path('', include('chatbot.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
